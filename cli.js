0445f0cded5a0566bd51eb48b72664a93d320507
Richard Littauer <richard.littauer@gmail.com>
2016-01-13 14:42:26 -0500

    Added check for upstream first

--- cli.js
+++ cli.js
@@ -2,6 +2,7 @@
 'use strict';
 const meow = require('meow');
 const gitRemoteOriginUrl = require('git-remote-origin-url');
+const gitRemoteUpstreamUrl = require('git-remote-upstream-url');
 const githubUrlFromGit = require('github-url-from-git');
 const opn = require('opn');
 
@@ -11,9 +12,13 @@ meow(`
 	  $ gh
 `);
 
-gitRemoteOriginUrl().then(url => {
+gitRemoteUpstreamUrl().then(url => {
 	opn(githubUrlFromGit(url), {wait: false});
 }).catch(() => {
-	console.error(`Couldn't find the remote origin URL. Ensure it's set and you're in a repo.\n\n  $ git remote add origin https://github.com/user/repo.git`);
-	process.exit(1);
+	gitRemoteOriginUrl().then(url => {
+		opn(githubUrlFromGit(url), {wait: false});
+	}).catch(() => {
+		console.error(`Couldn't find the remote origin or upstream URLs. Ensure they're set and you're in a repo.\n\n  $ git remote add origin https://github.com/user/repo.git`);
+		process.exit(1);
+	});
 });
