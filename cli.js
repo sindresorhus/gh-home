f8d221acc4b91eb0690cc2e1603f55a48de0714c
Aviskar KC <aviskarkc10@gmail.com>
2021-07-02 19:01:44 +0545

    Add flags for opening pull requests and issues of a GitHub repo (#10)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- cli.js
+++ cli.js
@@ -11,21 +11,51 @@ const cli = meow(`
 	Usage
 	  $ gh-home [repo | user/repo]
 
+	Options
+	  --prs -p	   Open the pull requests of a GitHub repo
+	  --issues -i  Open the issues of a GitHub repo
+	
 	Examples
 	  $ gh-home
 	  $ gh-home myrepo
 	  $ gh-home avajs/ava
-`);
+	  $ gh-home --issues
+	  $ gh-home --prs
+`, {
+	flags: {
+		prs: {
+			type: 'boolean',
+			default: false,
+			alias: 'p'
+		},
+		issues: {
+			type: 'boolean',
+			default: false,
+			alias: 'i'
+		}
+	}
+});
 
 const repo = cli.input[0];
+const options = cli.flags;
+
+const openUrl = (url, options) => {
+	if (options.prs) {
+		url = `${url}/pulls`;
+	} else if (options.issues) {
+		url = `${url}/issues`;
+	}
+
+	return open(url);
+};
 
 (async () => {
 	if (repo) {
 		if (repo.includes('/')) {
-			await open(`https://github.com/${repo}`);
+			await openUrl(`https://github.com/${repo}`, options);
 		} else {
 			const {stdout: user} = await execa('git', ['config', '--global', 'github.user']);
-			await open(`https://github.com/${user}/${repo}`);
+			await openUrl(`https://github.com/${user}/${repo}`, options);
 		}
 	} else {
 		let url;
@@ -47,6 +77,6 @@ const repo = cli.input[0];
 			process.exit(1);
 		}
 
-		await open(url);
+		await openUrl(url, options);
 	}
 })();
